baaf5c151e8a86c9616616eb613ffb93c9645b97
Sully Birashk <sullybirashk@Sullys-MBP.home>
2024-03-03 12:07:13 -0800

    Implement handleSubmit for form submission

--- src/Components/SignupForm/signupform.js
+++ src/Components/SignupForm/signupform.js
@@ -14,4 +14,16 @@ const handleChange = (e) => {
   setUserData({...userData, [e.target.name]: e.target.value});
 }
 
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    const response = await axios.post('http://localhost:3000/api/v1/users', {
+      user: userData
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export default SignupForm;
