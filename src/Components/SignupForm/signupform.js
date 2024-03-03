f2c4fb8b2bd6dca2dacdc5b13f8246e7c2dc3137
Sully Birashk <sullybirashk@Sullys-MBP.home>
2024-03-03 12:10:16 -0800

    Clean Up code

--- src/Components/SignupForm/signupform.js
+++ src/Components/SignupForm/signupform.js
@@ -9,33 +9,33 @@ function SignupForm() {
     password: '',
     password_confirmation: ''
   });
-}
 
-const handleChange = (e) => {
-  setUserData({...userData, [e.target.name]: e.target.value});
-}
-
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  try {
-    const response = await axios.post('http://localhost:3000/api/v1/users', {
-      user: userData
-    });
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
+  const handleChange = (e) => {
+    setUserData({...userData, [e.target.name]: e.target.value});
   }
-}
-return (
-  <form onSubmit={handleSubmit}>
-     <h2>Sign Up</h2>
-    <input type="text" name="name" placeholder="Name" onChange={handleChange} required />
-    <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
-    <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
-    <input type="password" name="password_confirmation" placeholder="Confirm Password" onChange={handleChange} required />
-    <button type="submit">Sign Up</button>
-  </form>
-);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/api/v1/users', {
+        user: userData // Corrected: directly pass userData here
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+       <h2>Sign Up</h2>
+      <input type="text" name="name" placeholder="Name" onChange={handleChange} required />
+      <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
+      <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
+      <input type="password" name="password_confirmation" placeholder="Confirm Password" onChange={handleChange} required />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
 
 export default SignupForm;
